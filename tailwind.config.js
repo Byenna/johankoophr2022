9b285a4bdec974e2956c96a1f9475901ba5edf21
Byenna <72694655+Byenna@users.noreply.github.com>
2022-10-29 22:39:32 +0200

    changed a lot

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,18 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx,vue}',
   ],
   theme: {
+   
+      screens : {
+        sm : '576px',
+        md : '768px',
+        lg : '992px',
+        xl : '1200px',
+      },
+      container : {
+        center : true,
+        padding : '1rem' // 16px
+      },
+    
     extend: {
       fontFamily: {
         'dancing': [
@@ -20,10 +32,17 @@ module.exports = {
         ],
     },
     fontSize: {
-      'Kooph1': ['2rem', { lineHeight: '2' }],//32
+      'Kooph1': ['2rem', { lineHeight: '1.2' }],//32
       'Kooph2': ['1.75rem', { lineHeight: '1' }],//28
       'Kooph3': ['1.5rem', { lineHeight: '1' }],//24px
       'Kooph-p': ['1rem', { lineHeight: '1.4rem' }],//16px
+    },
+    colors : {
+      'primary' : '#9b9db7',
+      'secondary' : '#044cac',
+      'tertiary' : '#5b7eb8',
+      'primary-light' : '#dde7f4',
+      
     },
   },
   },
